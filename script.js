2d0bfaf5622481c1c2692a6d40bd56beb92058d7
alisonycchu <69505082+alisonycchu@users.noreply.github.com>
2020-08-14 17:10:04 +0800

    update v4

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-/*function myFunction() {
+function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-}
-*/
-
-
-
-function button1(){
-  alert("You've been fooled, sweetie.");
 }
