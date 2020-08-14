63f434681d85a7767cf2874f847cf510fda5b3da
alisonycchu <69505082+alisonycchu@users.noreply.github.com>
2020-08-14 15:18:33 +0800

    portfile

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-var input = document.getElementById("text");
-var button = document.getElementById("send");
-var list = document.getElementById("list");
-
-button.addEventListener("click", addData);
-
-var todolist = [];
-
-function addData (){
-  todolist.push(input.value);
-  render();
+/*function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+*/
 
 
-function delData(index){
- var temp = [];
- for (var i = 0;  i< todolist.length; i ++){
-    if (i !=index)temp.push(todolist[i])
-   }
-  todolist = temp;
-  render();
+
+function button1(){
+  alert("You've been fooled, sweetie.");
 }
-
- function render(){
-   var listcontent ="";
-  for (var i=0; todolist.length > i; i++){
-    listcontent = listcontent + "<div><button onclick='delData(" + i + ")'>Delete</button>" + todolist[i] + "</div>";
-   }    
-  list.innerHTML = listcontent;
- }
